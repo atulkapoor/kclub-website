1aa1cc32b27372e520eb3a4f04c1152c7fa051b9
fauzan <fauzan@thewholetruthfoods.com>
2025-09-09 15:55:44 +0530

    updated-ui

--- src/app/services/[slug]/page.tsx
+++ src/app/services/[slug]/page.tsx
@@ -65,7 +65,7 @@ export default async function ServiceDetail({ params }: PageProps) {
             you to unlock efficiency, reduce costs, and gain a competitive edge
             in the digital-first era.
           </p>
-          <button className="px-5 py-2 bg-black text-white rounded hover:bg-gray-800 transition">
+          <button className="px-14 py-2 bg-black text-white rounded-lg bg-color-blue transition">
             Schedule a demo
           </button>
         </div>
@@ -73,6 +73,23 @@ export default async function ServiceDetail({ params }: PageProps) {
 
         <section className="container mx-auto px-6 py-16">
         <div className="grid md:grid-cols-2 gap-10 items-center">
+           {/* Right Image Placeholder */}
+          <div className="w-full h-64 bg-gray-300 flex items-center justify-center rounded-md">
+            <svg
+              className="w-12 h-12 text-gray-500"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M3 7l6 6 4-4 8 8M21 21H3V3h18v18z"
+              />
+            </svg>
+          </div>
           {/* Left Text */}
           <div>
             <h2 className="text-2xl md:text-3xl font-bold mb-4 text-gray-900">
@@ -90,23 +107,7 @@ export default async function ServiceDetail({ params }: PageProps) {
             </p>
           </div>
 
-          {/* Right Image Placeholder */}
-          <div className="w-full h-64 bg-gray-300 flex items-center justify-center rounded-md">
-            <svg
-              className="w-12 h-12 text-gray-500"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M3 7l6 6 4-4 8 8M21 21H3V3h18v18z"
-              />
-            </svg>
-          </div>
+         
         </div>
       </section>
 
@@ -114,22 +115,6 @@ export default async function ServiceDetail({ params }: PageProps) {
       <section className="container mx-auto px-6 py-16">
         <div className="grid md:grid-cols-2 gap-10 items-center">
           {/* Left Image Placeholder */}
-          <div className="w-full h-64 bg-gray-300 flex items-center justify-center rounded-md">
-            <svg
-              className="w-12 h-12 text-gray-500"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M3 7l6 6 4-4 8 8M21 21H3V3h18v18z"
-              />
-            </svg>
-          </div>
 
           {/* Right Text */}
           <div>
@@ -149,70 +134,90 @@ export default async function ServiceDetail({ params }: PageProps) {
               transform the way you work.
             </p>
           </div>
+
+            <div className="w-full h-64 bg-gray-300 flex items-center justify-center rounded-md">
+            <svg
+              className="w-12 h-12 text-gray-500"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M3 7l6 6 4-4 8 8M21 21H3V3h18v18z"
+              />
+            </svg>
+          </div>
         </div>
       </section>
 
       {/* Features Section */}
-            <section className="container mx-auto px-4 md:px-6 lg:px-36 py-12 lg:py-4 mt-20">
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8 text-center">
-                {service.features.map((feature, index) => (
-                  <div key={index} className="flex flex-col items-center">
-                    {/* Placeholder image */}
-                    <div className="w-50 h-50 bg-gray-200 flex items-center justify-center rounded mb-4">
-                      <Image
-                        src={feature.image}
-                        alt={feature.title}
-                        width={50}
-                        height={50}
-                        className="opacity-70"
-                      />
-                    </div>
-      
-                    <h3 className="text-2xl font-semibold text-gray-900">
-                      {feature.title}
-                    </h3>
-                    <p className="text-gray-600 text-xs md:text-sm mt-2">
-                      {feature.description}
-                    </p>
-                  </div>
-                ))}
-              </div>
-            </section>
-      
-     
-      
-            {/* CTA Section */}
-            <section className="container bg-gray-800 text-white mx-auto px-4 md:px-6 lg:px-36 py-12 lg:py-14 mt-20 rounded-md">
-              <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-6">
-                {/* Left Content */}
-                <div>
-                  <h2 className="text-3xl md:text-4xl font-bold mb-6 leading-tight subpixel-antialiased text-white">
-                    Explore Our Other Services, See Our Impact
-                  </h2>
-                  <p className="text-gray-300 mt-2 max-w-lg">
-                    Discover the full range of solutions we offer and explore case
-                    studies that showcase how we&apos;ve helped businesses transform
-                    through technology
-                  </p>
-                </div>
-      
-                {/* Right Buttons */}
-                <div className="flex gap-4">
-                  <a
-                    href="#case-studies"
-                    className="px-5 py-2 bg-black text-white rounded hover:bg-gray-700 transition"
-                  >
-                    See case studies
-                  </a>
-                  <a
-                    href="#contact"
-                    className="px-5 py-2 bg-black text-white rounded hover:bg-gray-700 transition"
-                  >
-                    Contact us
-                  </a>
-                </div>
-              </div>
-            </section>
+     <section className="container mx-auto px-4 md:px-6 py-12 lg:py-4 mt-20">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8">
+          {service.features.map((feature, index) => (
+            <div
+              key={index}
+              className="bg-white rounded-xl shadow-md p-8 flex flex-col text-center hover:shadow-lg transition"
+            >
+              {/* Image */}
+              {/* <div className="w-16 h-16 mx-auto bg-gray-100 flex items-center justify-center rounded-full mb-4">
+                <Image
+                  src={feature.image}
+                  alt={feature.title}
+                  width={40}
+                  height={40}
+                  className="opacity-80"
+                />
+              </div> */}
+
+              {/* Title */}
+              <h3 className="text-lg mb-8 font-semibold text-gray-900">
+                {feature.title}
+              </h3>
+
+              {/* Description */}
+              <p className="text-gray-600 text-sm mt-2">
+                {feature.description}
+              </p>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      <section className="container bg-color-blue mb-10 text-white mx-auto px-4 md:px-6 lg:px-36 py-12 lg:py-14 mt-20 rounded-md">
+        <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-6">
+          {/* Left Content */}
+          <div>
+            <h2 className="text-3xl md:text-4xl font-bold mb-6 leading-tight subpixel-antialiased text-white">
+              Explore Our Other Services, See Our Impact
+            </h2>
+            <p className="text-gray-300 mt-2 max-w-lg">
+              Discover the full range of solutions we offer and explore case
+              studies that showcase how we&apos;ve helped businesses transform
+              through technology
+            </p>
+          </div>
+
+          {/* Right Buttons */}
+          <div className="flex gap-4">
+          <a
+            href="#case-studies"
+            className="px-10 py-2 bg-white text-black rounded-full border border-black hover:bg-gray-100 transition"
+          >
+            See case studies
+          </a>
+          <a
+            href="#contact"
+            className="px-10 py-2 bg-white text-black rounded-full border border-black hover:bg-gray-100 transition"
+          >
+            Contact us
+          </a>
+        </div>
+        </div>
+      </section>
     </main>
   );
 }
